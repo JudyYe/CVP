68a04c03a99bb46ba44a6f7262388b3fc11816c9
JudyYe <flowernancyye@sina.com>
2019-06-08 16:44:16 -0400

    docs

--- doc/src/gym/make_list.py
+++
@@ -1,9 +0,0 @@
-import glob
-
-all_list = glob.glob('*.gif')
-
-fname = 'demo_list.txt'
-with open(fname, 'w') as fp:
-    for each in all_list:
-        index = each.split('_')[0]
-        fp.write('%s\n' % index)
